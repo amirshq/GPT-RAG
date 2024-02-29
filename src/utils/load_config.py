ea21aaffed74a2b8a5c1d25e564aca907d18d89e
Amir Shahcheraghian <amir.shahcheraghian@gmail.com>
2024-02-29 08:50:52 -0500

    load config

--- src/utils/load_config.py
+++ src/utils/load_config.py
@@ -13,4 +13,20 @@ class LoadConfig:
         with open(here("configs/app_config.yml")) as cfg:
             app_config= yaml.load(cfg,loader=yaml.FullLoader)
         #LLM configs
+        self.llm_engine = app_config["llm_config"]["engine"]
+        self.llm_system_role = app_config["llm_config"]["llm_system_role"]
+        self.persist_directory = str(here(
+            app_config["directories"]["persist_directory"]))
+        self.custom_perist_directory = str(here(
+            app_config["directories"]["custom_persist_directory"]))
+        self.embedding_model = OpenAIEmbeddings()
+
+        #Retrieval Configs 
+        self.data_directory = app_config["directories"]["data_directory"]
+        self.k = app_config["retrieval_config"]["k"]
+        self.embedding_model_engine = app_config["embedding_model_config"]["engine"]
+        self.chunk_size = app_config["splitter_config"]["chunk_size"]
+        self.chunk_overlap = app_config["splitter_config"]["chunk_overlap"]
+
+        #summarization configs
         
