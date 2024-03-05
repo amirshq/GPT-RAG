e23a84068f4a32d6407a6c669928ef92e2df39f8
Amir Shahcheraghian <amir.shahcheraghian@gmail.com>
2024-03-05 09:14:31 -0500

    Terminal Q&A

---
+++ src/Terminal_Q_and_A.py
@@ -0,0 +1,25 @@
+"""
+    this code snippet demonstrates an interactive Q&A system where
+    users can input questions, and the system retrieves relevant 
+    information from a vector database and generates responses using 
+    an AI language model provided by OpenAI.
+"""
+import openai 
+import yaml 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import chroma
+from typing  import list, Tuple 
+from utils.load_config import LoadConfig
+
+#Loading openai credentials
+APPCFG = LoadConfig()
+
+with open("configs/app_config.yml") as cfg:
+    app_config= yaml.load(cfg,loader=yaml.FullLoader)
+
+#Load the mebedding function
+embedding = OpenAIEmbeddings()
+#Load the vector database
+vectordb = chroma(persist_directory=APPCFG.persist_directory,embedding_function=embedding)
+
+print("Number of vectors in the vector database: ",vectordb._collection.count())
